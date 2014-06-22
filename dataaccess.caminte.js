4627e97b53f45ff1b2d05948602f9078472b0a5b
Ryan Tharp <email@notavailable.com>
2014-06-22 08:31:39 -0700

    Data models

--- dataaccess.caminte.js
+++ dataaccess.caminte.js
@@ -11,6 +11,196 @@ var Schema = require('caminte').Schema;
 // so MySQL is good alternative if you want a large dataset
 // SQLite is a good incase the user doesn't want to install any extra software
 
+// set up the (eventually configureable) model pools
+// 6379 is default redis port number
+var schemaData = new Schema('redis', {port: 6379}); //port number depends on your configuration
+
+// Auth models and accessors can be moved into own file?
+// so that routes.* can access them separately from everything!
+
+// NOTE: all models automically have a default 'id' field that's an AutoIncrement
+
+/*
+ * Data Models
+ */
+
+// this data needs to not use internal Pks
+// I'd like to be able to copy random tables from one server to another
+// to help bootstrap caches
+
+var clientModel = schemaData.define('Client', {
+  client_id: { type: String, limit: 32, index: true }, // probably should be client_id
+  secret: { type: String, limit: 32 },
+  userid: { type: Number },
+  name: { type: String, limit: 255 },
+  link: { type: String, limit: 255 }
+});
+clientModel.validatesUniquenessOf('client_id', {message:'client_id is not unique'});
+
+var userModel = schemaData.define('User', {
+  /* API START */
+  username: { type: String, length: 21, index: true },
+  name: { type: String, length: 50 },
+  description: { type: schemaData.Text },
+  descriptionhtml: { type: schemaData.Text }, /* cache */
+  created_at: { type: Date },
+  timezone: { type: String, length: 64 },
+  // maybe remove this?
+  locale: { type: String, length: 16 },
+  // this will need to change, so we can support multiples?
+  avatar_image: { type: String, length: 255 },
+  avatar_width: { type: Number } ,
+  avatar_height: { type: Number } ,
+  cover_image: { type: String, length: 255 },
+  cover_width: { type: Number } ,
+  cover_height: { type: Number } ,
+  // is_default?
+  following: { type: Number, default: 0 }, /* cache */
+  followers: { type: Number, default: 0 }, /* cache */
+  posts: { type: Number, default: 0 }, /* cache */
+  stars: { type: Number, default: 0 }, /* cache */
+  deleted: { type: Date },
+  type: { type: String, length: 32 },
+  canonical_url: { type: String, length: 255 },
+  verified_domain: { type: String, length: 255 },
+  verified_link: { type: String, length: 255 },
+  /* API END */
+  last_updated: { type: Date },
+  stars_updated: { type: Date },
+  language: { type: String, length: 2 },
+  country: { type: String, length: 2 },
+});
+userModel.validatesUniquenessOf('username', {message:'username is not unique'});
+
+var postModel = schemaData.define('post', {
+  userid: { type: Number, index: true },
+  text: { type: schemaData.Text },
+  html: { type: schemaData.Text }, /* cache */
+  machine_only: { type: Boolean, default: false, index: true },
+  reply_to: { type: Number }, // kind of want to index this
+  thread_id: { type: Number, index: true },
+  is_deleted: { type: Boolean, default: false, index: true },
+  created_at: { type: Date, index: true },
+  client_id: { type: String, length: 32, index: true },
+  repost_of: { type: Number, default: 0, index: true },
+  canonical_url: { type: String },
+  num_replies: { type: Number, default: 0 },
+  num_reposts: { type: Number, default: 0 },
+  num_stars: { type: Number, default: 0 }
+});
+
+// total cache table, we'll have an option to omitted its use
+// though we need it for hashtag look ups
+var entityModel = schemaData.define('entity', {
+  idtype: { type: String, length: 16, index: true }, // user, post, channel, message
+  typeid: { type: Number, index: true }, // causing problems?
+  type: { type: String, length: 16, index: true }, // link, hashtag, mention
+  pos: { type: Number },
+  len: { type: Number },
+  text: { type: String, length: 255, index: true }, // hashtag is stored here
+  alt: { type: String, length: 255, index: true },
+  altnum: { type: Number },
+});
+
+var annotationModel = schemaData.define('annotation', {
+  idtype: { type: String, index: true }, // user, post, channel, message
+  typeid: { type: Number, index: true }, // causing problems?
+  type: { type: String, length: 255, index: true },
+  value:  { type: schemaData.JSON },
+});
+
+// maybe not needed with JSON type
+var annotationValuesModel = schemaData.define('annotationvalues', {
+  annotationid: { type: Number, index: true },
+  key: { type: String, length: 255, index: true },
+  value: { type: schemaData.Text }, // kind of want to index this
+  memberof: { type: Number, index: true }
+});
+
+var channelModel = schemaData.define('channel', {
+  ownerid: { type: Number, index: true },
+  type: { type: String, length: 255, index: true },
+  reader: { type: Number }, // 0=public,1=loggedin,2=selective
+  writer: { type: Number }, // 1=loggedin,2=selective
+  // editors are always seletcive
+  readedit: { type: Boolean, default: true }, // immutable?
+  writeedit: { type: Boolean, default: true }, // immutable?
+  editedit: { type: Boolean, default: true }, // immutable?
+  // could be store as json, since we're parsing either way...
+  readers: { type: schemaData.Text }, // comma separate list
+  writers: { type: schemaData.Text }, // comma separate list
+  editors: { type: schemaData.Text }, // comma separate list
+  created_at: { type: Date }, // created_at isn't in the API
+  last_updated: { type: Date },
+});
+
+var messageModel = schemaData.define('message', {
+  channel_id: { type: Number, index: true },
+  html: { type: schemaData.Text }, /* cache */
+  text: { type: schemaData.Text },
+  machine_only: { type: Boolean, index: true },
+  client_id: { type: String, length: 32 },
+  thread_id: { type: Number, index: true },
+  userid: { type: Number, index: true },
+  reply_to: { type: Number }, // kind of want to index this
+  is_deleted: { type: Boolean, index: true },
+  created_at: { type: Date, index: true },
+});
+
+var subscriptionModel = schemaData.define('subscriptions', {
+  channelid: { type: Number, index: true },
+  userid: { type: Number, index: true },
+  created_at: { type: Date, index: true },
+  active: { type: Boolean, index: true },
+  last_updated: { type: Date },
+});
+
+var followModel = schemaData.define('follow', {
+  userid: { type: Number, index: true },
+  followsid: { type: Number, index: true }, // maybe not index this
+  active: { type: Boolean, index: true },
+  // aka pagenationid, we'll need this for meta.id too
+  referenceid: { type: Number, index: true }, // this only exists in meta.id and is required for deletes for app streaming
+  created_at: { type: Date, index: true }, // or this
+  last_updated: { type: Date },
+});
+
+// split up?
+// we don't need reposts here becauses have all that info with a repost_of column
+// since an entire post is created on repost
+// though repost could also write here in the future, making it easier to pull
+var interactionModel = schemaData.define('interaction', {
+  userid: { type: Number, index: true },
+  type: { type: String, length: 8, index: true }, // star,unstar,repost,unrepost
+  datetime: { type: Date },
+  idtype: { type: String, index: true }, // post (what about chnl,msg,user? not for existing types)
+  typeid: { type: Number, index: true }, // causing problems?
+  asthisid: { type: Number } // meta.id
+});
+
+var fileModel = schemaData.define('file', {
+  /* API START */
+  userid: { type: Number, index: true },
+  client_id: { type: String, length: 32 },
+  kind: { type: String, length: 255, index: true },
+  name: { type: String, length: 255 },
+  type: { type: Number, index: true }, // com.example.test
+  complete: { type: Boolean },
+  sha1: { type: String, length: 255 },
+  url: { type: String, length: 512 },
+  total_size: { type: Number },
+  size: { type: Number },
+  mime_type: { type: String, length: 255 },
+  urlexpires: { type: Date },
+  /* API END */
+  last_updated: { type: Date },
+});
+
+// Auth Todo: localUser, localClient
+// Token Todo: userToken, appToken
+// Rate Todo: userTokenLimit, appTokenLimit
+// Data Todo: mutes, blocks, upstream_tokens
+
 module.exports = {
   next: null,
   /*
