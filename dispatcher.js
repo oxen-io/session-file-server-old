88a67e69e2674b43080290528c8cd8137996d453
Ryan Tharp <email@notavailable.com>
2014-06-22 08:12:29 -0700

    *ToAPI functions, more implementation glue
    Created User/Post ToAPI wrapper to convert storage format to internal
    format. Implemented: getPost, getGlobal, getUserPosts, getUserStars,
    getHashtag, getChannel, getChannelMessages

--- dispatcher.js
+++ dispatcher.js
@@ -9,27 +9,133 @@ module.exports = {
   /** posts */
   setPost: function(post, callback) {
     console.log('dispatcher.js::setPost - write me!');
-    callback(null, null);
+    if (callback) {
+      callback(null, null);
+    }
+  },
+  // convert DB format to API structure
+  postToAPI: function(post,callback,meta) {
+    console.log('dispatcher.js::postToAPI - write me!');
+    callback(post, null);
   },
   getPost: function(id, params, callback) {
-    console.log('dispatcher.js::getPost - write me!');
-    callback(null, null);
+    // probably should just exception and backtrace
+    if (callback==undefined) {
+      console.log('dispatcher.js::getPost - callback undefined');
+      return;
+    }
+    if (id==undefined) {
+      callback(null,'dispatcher.js::getPost - id is undefined');
+    }
+    var ref=this;
+    this.cache.getPost(id, function(post, err, meta) {
+      ref.postToAPI(post, callback, meta)
+    });
   },
   getGlobal: function(params, callback) {
-    console.log('dispatcher.js::getGlobal - write me!');
-    callback(null, null);
+    var ref=this;
+    this.cache.getGlobal(function(posts,err) {
+      // data is an array of entities
+      var apiposts=[];
+      //console.log('dispatcher.js:getGlobal - mapping '+posts.length);
+      if (posts.length) {
+        posts.map(function(current,idx,Arr) {
+          //console.log('dispatcher.js:getGlobal - map postid: '+current.id);
+          // get the post in API foromat
+          ref.postToAPI(current,function(post,err,meta) {
+            apiposts.push(post);
+            // join
+            //console.log(apiposts.length+'/'+entities.length);
+            if (apiposts.length==posts.length) {
+              //console.log('dispatcher.js::getGlobal - finishing');
+              callback(apiposts);
+            }
+          });
+        },ref);
+      } else {
+        // no posts
+        callback(null,'no posts for global',meta);
+      }
+    });
   },
   getUserPosts: function(userid, params, callback) {
-    console.log('dispatcher.js::getUserPosts - write me!');
-    callback(null, null);
+    var ref=this;
+    this.cache.getUserPosts(userid, function(posts, err, meta) {
+      // data is an array of entities
+      var apiposts=[];
+      //console.log('dispatcher.js:getGlobal - mapping '+posts.length);
+      if (posts && posts.length) {
+        posts.map(function(current,idx,Arr) {
+          //console.log('dispatcher.js:getGlobal - map postid: '+current.id);
+          // get the post in API foromat
+          ref.postToAPI(current,function(post,err,meta) {
+            apiposts.push(post);
+            // join
+            //console.log(apiposts.length+'/'+entities.length);
+            if (apiposts.length==posts.length) {
+              //console.log('dispatcher.js::getGlobal - finishing');
+              callback(apiposts);
+            }
+          });
+        },ref);
+      } else {
+        // no posts
+        callback([],'no posts for global',meta);
+      }
+    });
   },
   getUserStars: function(userid, params, callback) {
-    console.log('dispatcher.js::getUserStars - write me!');
-    callback(null, null);
+    //console.log('dispatcher.js::getUserStars start');
+    var ref=this;
+    this.cache.getInteractions('star', userid, function(interactions, err, meta) {
+      //console.log('dispatcher.js::getUserStars - ',interactions);
+      // data is an array of interactions
+      if (interactions.length) {
+        var apiposts=[];
+        interactions.map(function(current,idx,Arr) {
+          // we're a hasMany, so in theory I should be able to do
+          // record.posts({conds});
+          // get the post in API foromat
+          ref.getPost(current.typeid, null, function(post, err, meta) {
+            apiposts.push(post);
+            // join
+            //console.log(apiposts.length+'/'+interactions.length);
+            if (apiposts.length==interactions.length) {
+              //console.log('dispatcher.js::getUserStars - finishing');
+              callback(apiposts);
+            }
+          });
+        });
+      } else {
+        callback(interactions, err, meta);
+      }
+    });
   },
   getHashtag: function(hashtag, params, callback) {
-    console.log('dispatcher.js::getHashtag - write me!');
-    callback(null, null);
+    var ref=this;
+    //console.log('dispatcher.js:getHashtag - start #'+hashtag);
+    this.cache.getHashtagEntities(hashtag,function(entities,err,meta) {
+      // data is an array of entities
+      var apiposts=[];
+      //console.log('dispatcher.js:getHashtag - mapping '+entities.length);
+      if (entities.length) {
+        entities.map(function(current,idx,Arr) {
+          // get the post in API foromat
+          ref.getPost(current.typeid, null, function(post,err,meta) {
+            apiposts.push(post);
+            // join
+            //console.log(apiposts.length+'/'+entities.length);
+            if (apiposts.length==entities.length) {
+              //console.log('dispatcher.js::getHashtag - finishing');
+              callback(apiposts);
+            }
+          });
+        },ref);
+      } else {
+        // no entities
+        callback(null,'no entities for '+hashtag,meta);
+      }
+    });
   },
   /** channels */
   setChannel: function(json, ts, callback) {
@@ -37,16 +143,14 @@ module.exports = {
     callback(null, null);
   },
   getChannel: function(id, params, callback) {
-    console.log('dispatcher.js::getChannel - write me!');
-    callback(null, null);
+    this.cache.getChannel(id, callback);
   },
   /** messages */
   setMessage: function(json, ts) {
     console.log('dispatcher.js::setMessage - write me!');
   },
   getChannelMessages: function(cid, params, callback) {
-    console.log('dispatcher.js::getChannelMessages - write me!');
-    callback(null, null);
+    this.cache.getChannelMessages(cid, callback);
   },
   getChannelMessage: function(cid, mids, params, callback) {
     console.log('dispatcher.js::getChannelMessage - write me!');
@@ -73,6 +177,10 @@ module.exports = {
   updateUser: function(data, ts, callback) {
     console.log('dispatcher.js::updateUser - write me!');
     callback(null, null);
+  },
+  userToAPI: function(user,callback,meta) {
+    console.log('dispatcher.js::userToAPI - write me!');
+    callback(user, null);
   },
   getUser: function(id, params, callback) {
     console.log('dispatcher.js::getUser - write me!');
