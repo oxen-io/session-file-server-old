9715975c89e7c4c0952f808655d7bb2e9fb6e1cc
Ryan Tharp <email@notavailable.com>
2014-06-22 17:32:46 -0700

    textProcess implementation
    a start of textProcess, edge cases need work

--- dispatcher.js
+++ dispatcher.js
@@ -796,8 +796,144 @@ module.exports = {
   },
   /** text process */
   textProcess: function(text, entities, postcontext, callback) {
-    console.log('dispatcher.js::textProcess - write me!');
-    callback(null, null);
+    var ref=this;
+    var html=text;
+    var mentions=[];
+    var hashtags=[];
+    var links=[];
+    // from patter @duerig
+    // FIXME: these text ranges aren't very i8n friendly, what about UTF stuff huh?
+    var mentionRegex = /@([a-zA-Z0-9\-_]+)\b/g;
+    var hashtagRegex = /#([a-zA-Z0-9\-_]+)\b/g;
+    // https://gist.github.com/gruber/8891611
+    // https://alpha.app.net/dalton/post/6597#6595
+    var urlRegex = /\b((?:https?:(?:\/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b\/?))/ig;
+
+    // data-mention-id="$1" will have to do a look up pass and set this back
+    html = html.replace(mentionRegex,'<span data-mention-name="$1" itemprop="mention">@$1</span>');
+    html = html.replace(hashtagRegex,'<span data-hashtag-name="$1" itemprop="hashtag">#$1</span>');
+
+    // since input is text, I believe we can safely assume it's not already in a tag
+    // FIXME: we need to insert http:// if there's no protocol (post: 30795290)
+    // be sure to check your html/entity caching to make sure it's off otherwise 30795290 is fine
+    html = html.replace(urlRegex,'<a href="$1">$1</a>');
+
+    var userlookup={};
+
+    var finishcleanup=function(html,text,callback) {
+      if (!entities) {
+        var lastmenpos=0;
+        while(match=mentionRegex.exec(text)) {
+          //console.log('Found '+match[1]+' at '+match.index);
+          var username=match[1].toLowerCase();
+          //console.log('@'+match.index+' vs '+lastmenpos);
+          var obj={
+            pos: match.index,
+            id: ''+userlookup[username],
+            len: username.length+1, // includes char for @
+            name: username,
+          }
+          if (postcontext) {
+            // means no text before the mention...
+            obj.is_leading=match.index==lastmenpos;
+          }
+          mentions.push(obj);
+          // while we're matching
+          if (match.index==lastmenpos) {
+            // update it
+            lastmenpos=match.index+username.length+2; // @ and space after wards
+          }
+        }
+        // FIXME: 30792555 invisible hashtags?
+        // we're not encoding text right...
+        while(match=hashtagRegex.exec(text)) {
+          var hashtag=match[1];
+          var obj={
+            name: hashtag,
+            pos: match.index,
+            len: hashtag.length+1, // includes char for #
+          }
+          hashtags.push(obj);
+        }
+        while(match=urlRegex.exec(text)) {
+          var url=match[1];
+          // we need to insert http:// if there's no protocol (post: 30795290)
+          // FIXME: colon isn't good enough
+          var text=url;
+          if (url.indexOf(':')==-1) {
+            url='http://'+url;
+          }
+          var obj={
+            url: url,
+            text: text,
+            pos: match.index,
+            len: text.length,
+          }
+          links.push(obj);
+        }
+
+        /*
+        console.dir(mentions);
+        console.dir(hashtags);
+        console.dir(links);
+        */
+
+        entities={
+          mentions: mentions,
+          hashtags: hashtags,
+          links: links
+        };
+      }
+
+      // unicode chars
+      // <>\&
+      html = html.replace(/[\u00A0-\u9999]/gim, function(i) {
+         return '&#'+i.charCodeAt(0)+';';
+      });
+
+      // remove line breaks
+      html=html.replace(/\r/g,'&#13;');
+      html=html.replace(/\n/g,'<br>');
+
+      var res={
+        entities: entities,
+        html: '<span itemscope="https://app.net/schemas/Post">'+html+'</span>',
+        text: text
+      };
+      callback(res,null);
+    }
+
+    var mentionsSrch=text.match(mentionRegex);
+    var launches=0,completed=0;
+    if (mentionsSrch && mentionsSrch.length) {
+      for(var i in mentionsSrch) {
+        var mention=mentionsSrch[i]; // with @
+        //RegExp.$1 // without @
+        //var username=RegExp.$1;
+        var username=mention.substr(1);
+        //console.log("Replacing "+username);
+        launches++;
+        ref.cache.getUserID(username,function(user,userErr,userMeta) {
+          //console.log('Searching for '+user.username);
+          userlookup[user.username]=user.id;
+          // fix up missing user ids
+          var pattern=new RegExp(' data-mention-name="'+user.username, 'gi');
+          html=html.replace(pattern,' data-mention-id="'+user.id+'" data-mention-name="'+user.username);
+          //console.log('Adjusted html '+html);
+          completed++;
+          //console.log(completed+'/'+launches);
+          // tired/lazy man's promise
+          // I'm concerned that if we queue 2 and then finish 2, we may trigger the ending early
+          // and possibly more than once
+          if (completed==launches) {
+            finishcleanup(html,text,callback);
+          }
+        });
+      }
+    } else {
+      finishcleanup(html,text,callback);
+    }
+
   },
   /** dispatcher for streamrouter */
   dispatch: function(userid, json) {
